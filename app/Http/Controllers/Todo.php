7b104091fb5c006e10e69c54d8b07eea67ef6178
saeed.hassan <saeed.hassan@paysyslabs.com>
2023-06-03 19:45:44 +0500

    initial commit

--- app/Http/Controllers/Todo.php
+++ app/Http/Controllers/Todo.php
@@ -9,108 +9,14 @@ use Symfony\Component\HttpFoundation\Request;
 
 class Todo extends BaseController
 {
-
-
-  public function showHomePage()
+  public function showTodos()
   {
-    return view('todo_show');
+    return view('todo_show', ['todos' => DB::select('select * from todo')]);
   }
 
-  public function getAllTodos()
+  public function showForm()
   {
-    return Response::json(array('todos' => DB::select('select * from todo')));
-  }
-
-  public function createTodo(Request $req)
-  {
-
-    $req->validate([
-      'title' => ['required', 'min:5', 'max:30'],
-      'description' => [
-        'required',
-        'min:5',
-      ],
-    ]);
-
-    $title = $req->title;
-    $desc = $req->description;
-    $query = 'insert into todo (task_title, task_description) values (?, ?)';
-
-    try {
-      DB::insert($query, [$title, $desc]);
-
-      return Response::json(
-        array('message' => 'success'), status: 201
-      );
-    } catch (\Throwable $th) {
-      return Response::json(
-        array(
-          'message' => $th->getMessage()
-        ),
-        status: $th->getCode()
-      );
-    }
-  }
-
-  public function getSingleTodo($id)
-  {
-    return Response::json(['todo' => DB::table('todo')->where('id', $id)->first()], status: 200);
-  }
-
-  public function todoUpdate(Request $req)
-  {
-    $req->validate([
-      "todo_id" => ['required'],
-      'title' => ['required', 'string', 'min:5', 'max:30'],
-      'description' => ['required', 'min:5', 'string'],
-      'status' => ['required']
-    ]);
-
-    dd($req);
-
-
-    $id = $req->todo_id;
-    $title = $req->title;
-    $desc = $req->description;
-    $status = $req->status;
-
-    $query = 'update todo set task_title=?, task_description=?, is_task_complete=? where id = ?';
-
-    try {
-      // dd($status);
-      DB::update($query, [$title, $desc, $status, $id]);
-
-      return redirect('/');
-    } catch (\Throwable $th) {
-      return $th->getMessage();
-    }
-  }
-
-  public function editTodo(Request $req)
-  {
-    $req->validate([
-      "todo_id" => ['required'],
-      'title' => ['required', 'string', 'min:5', 'max:30'],
-      'description' => ['required', 'min:5', 'string'],
-      'status' => ['required']
-    ]);
-
-    $id = $req->todo_id;
-    $title = $req->title;
-    $desc = $req->description;
-    $status = $req->status;
-
-    $query = 'update todo set task_title=?, task_description=?, is_task_complete=? where id = ?';
-
-    try {
-      // dd($status);
-      DB::update($query, [$title, $desc, $status, $id]);
-
-      // return redirect('/');
-      return Response::json(array('message' => 'Todo Updated Successfully'), status: 200);
-    } catch (\Throwable $th) {
-      return $th->getMessage();
-    }
+    return view('todo_create');
   }
 
   public function todoDelete($id)
@@ -134,78 +40,51 @@ class Todo extends BaseController
     }
   }
 
-  public function deleteTodo($id)
+  public function todoUpdate(Request $req)
   {
+    $req->validate([
+      "todo_id" => ['required'],
+      'title' => ['required', 'string', 'min:5', 'max:30'],
+      'description' => ['required', 'min:5', 'string'],
+      'status' => ['required']
+    ]);
+
+
+    $id = $req->todo_id;
+    $title = $req->title;
+    $desc = $req->description;
+    $status = $req->status;
+
+    $query = 'update todo set task_title=?, task_description=?, is_task_complete=? where id = ?';
 
     try {
-      $deleted = DB::delete('delete from todo where id = ?', [$id]);
-
-      if (!$deleted) {
-        return Response::json(array('message' => "Todo not found"), status: 404);
-      }
-
-      return Response::json(
-        array(
-          'message' => "Todo Deleted Successfully",
-        ),
-      );
-
-
+      DB::update($query, [$title, $desc, $status, $id]);
+      return redirect('/');
     } catch (\Throwable $th) {
-      return Response::json(
-        array(
-          'message' => $th->getMessage(),
-          'statusCode' => $th->getCode()
-        ), status: $th->getCode()
-      );
+      return $th->getMessage();
     }
   }
-
-
   public function todoShowUpdateForm($id)
   {
     return view('todo_update', ['todo' => DB::table('todo')->where('id', $id)->first()]);
   }
 
-  // public function showForm()
-  // {
-  //   return view('todo_create');
-  // }
+  public function addTodo(Request $req)
+  {
+    $req->validate([
+      'title' => ['required', 'max:30'],
+      'description' => ['required'],
+    ]);
 
+    $title = $req->title;
+    $desc = $req->description;
+    $query = 'insert into todo (task_title, task_description) values (?, ?)';
 
-  // public function showTodos()
-  // {
-  //   return view('todo_show', ['todos' => DB::select('select * from todo')]);
-  // }
-
-  // public function addTodo(Request $req)
-  // {
-
-
-  //   $req->validate([
-  //     'title' => ['required', 'max:30'],
-  //     'description' => ['required'],
-  //   ]);
-
-  //   $title = $req->title;
-  //   $desc = $req->description;
-  //   $query = 'insert into todo (task_title, task_description) values (?, ?)';
-
-  //   try {
-  //     DB::insert($query, [$title, $desc]);
-  //     return redirect('/');
-  //   } catch (\Throwable $th) {
-  //     return $th->getMessage();
-  //   }
-  // }
-
-  // 
-
-  // public function show()
-  // {
-  //   return 'hello testing';
-  // }
-
-  // echo $req->getContent();
-  // printr($req->all());
+    try {
+      DB::insert($query, [$title, $desc]);
+      return redirect('/');
+    } catch (\Throwable $th) {
+      return $th->getMessage();
+    }
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,60 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Todo;
 
-// ---------- Routes with Ajax Request ------------------------------------
-Route::get('/', [Todo::class, 'showHomePage']);
-Route::get('/getAllTodos', [Todo::class, 'getAllTodos'])->name('list_todos');
-Route::get('/todos/{id}', [Todo::class, 'getSingleTodo'])->name('get_single_todo');
-Route::post('/todos', [Todo::class, 'createTodo'])->name('add_single_todo');
-Route::get('/todos/delete/{id}', [Todo::class, 'deleteTodo'])->name('delete_single_todo');
-Route::post('/todos/edit/', [Todo::class, 'editTodo'])->name('edit_single_todo');
-
-// ------------ Routes without Ajax ---------------------------------------
-// Route::get('/', [Todo::class, 'showTodos']);
-// Route::get('/todo-form', [Todo::class, 'showForm']);
-// Route::get('/todos/delete/{id}', [Todo::class, 'todoDelete']);
-// Route::post('/todos/update_status/', [Todo::class, 'updateStatus']);
-// Route::put('todos/{id}', 'TodoController@update');
-// Route::post('/todos/edit/', [Todo::class, 'todoUpdate']);
-// Route::get('/todos/{id}', [Todo::class, 'todoShowUpdateForm']);
-// Route::post('/todos', [Todo::class, 'addTodo']);
-
-// ---------- for learning purposes ------------------------
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// Route::get('/', function () {
-//     return 'test get req';
-// });
-
-// Route::post('/test', function() {
-//     return 'test post req';
-// });
-
-// Route::put('/test', function() {
-//     return 'test put req';
-// });
-
-// Route::patch('/test', function() {
-//     return 'test patch req';
-// });
-
-// Route::delete('/test', function() {
-//     return 'test delete req';
-// });
-
-
-// Route::view('/home', 'name');  
-// Route::view('/home', 'name', ['name' => 'Taylor']);
-
-
-// Route::get('/user/{userId}', function (string $id) {
-//     return $id;
-// });
-
-
-
-// Route::get('/user/{userId}/{userName}', function (string $id, string $name) {
-//     return $id . ' ' . $name;
-// });
+Route::get('/', [Todo::class, 'showTodos']);
+Route::get('/todo-form', [Todo::class, 'showForm']);
+Route::get('/todos/delete/{id}', [Todo::class, 'todoDelete']);
+Route::post('/todos/update_status/', [Todo::class, 'updateStatus']);
+Route::put('todos/{id}', 'TodoController@update');
+Route::post('/todos/edit/', [Todo::class, 'todoUpdate']);
+Route::get('/todos/{id}', [Todo::class, 'todoShowUpdateForm']);
+Route::post('/todos', [Todo::class, 'addTodo']);
